48855c0be3489fef7b65e84675f49f3b70fd941b
camargo-devs <camargo.bruno.dev@gmail.com>
2021-05-30 14:36:15 -0300

    Finish admin product card layout

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Card from '../Card';
 
 const List = () => {
     const history = useHistory();
 
-        const handleCreate = () => {
-            history.push('/admin/products/create');
-        }
+    const handleCreate = () => {
+        history.push('/admin/products/create');
+    }
 
     return (
         <div className="admin-products-list">
             <button className="btn btn-primary btn-lg" onClick={handleCreate}>
                 ADICIONAR
             </button>
+            <div className="admin-list-container">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     );
 }
