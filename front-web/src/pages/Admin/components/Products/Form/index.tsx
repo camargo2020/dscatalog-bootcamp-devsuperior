00906b757de7dc9caa239b36f6198e50f5e2d6ae
camargo-devs <camargo.bruno.dev@gmail.com>
2021-06-09 12:09:07 -0300

    Update react-hook-form

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,16 +1,19 @@
 import { makePrivateRequest, makeRequest } from 'core/utils/request';
-import { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router';
 import { toast } from 'react-toastify';
+import Select from 'react-select';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
+import { Category } from 'core/types/Product';
 
 type FormState = {
     name: string;
     price: string;
     description: string;
     imgUrl: string;
+    categories: Category[];
 }
 
 type ParamsType = {
@@ -18,29 +21,38 @@ type ParamsType = {
 }
 
 const Form = () => {
-    const { register, handleSubmit, formState: { errors }, setValue } = useForm<FormState>();
+    const { register, handleSubmit, formState: { errors }, setValue, control } = useForm<FormState>();
     const history = useHistory();
     const { productId } = useParams<ParamsType>();
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+    const [categories, setCategories] = useState<Category[]>([]);
     const isEditing = productId !== 'create';
     const formTitle = isEditing ? 'Editar produto' : 'Cadastrar produto'
-
+    
     useEffect(() => {
         if (isEditing) {
             makeRequest({ url: `/products/${productId}` })
-            .then(response => {
-                setValue('name', response.data.name);
-                setValue('price', response.data.price);
-                setValue('description', response.data.description);
-                setValue('imgUrl', response.data.imgUrl);
-            })
+                .then(response => {
+                    setValue('name', response.data.name);
+                    setValue('price', response.data.price);
+                    setValue('description', response.data.description);
+                    setValue('imgUrl', response.data.imgUrl);
+                })
         }
     }, [productId, isEditing, setValue]);
 
+    useEffect(() => {
+        setIsLoadingCategories(true);
+        makeRequest({ url: '/categories' })
+            .then(response => setCategories(response.data.content))
+            .finally(() => setIsLoadingCategories(false));
+    }, []);
+
     const onSubmit = (data: FormState) => {
         console.log(data)
-        makePrivateRequest({ 
-            url: isEditing ? `/products/${productId}` : '/products', 
-            method: isEditing ? 'PUT' : 'POST', 
+        makePrivateRequest({
+            url: isEditing ? `/products/${productId}` : '/products',
+            method: isEditing ? 'PUT' : 'POST',
             data
         })
             .then(() => {
@@ -62,9 +74,9 @@ const Form = () => {
                         <div className="margin-botton-30">
                             <input
                                 {...register('name', {
-                                    required: "Campo obrigatório", 
-                                    minLength: { value: 5, message: "O campo deve ter no mínimo 5 caracteres!"},
-                                    maxLength: { value: 60, message: "O campo deve ter no mínimo 60 caracteres!"}
+                                    required: "Campo obrigatório",
+                                    minLength: { value: 5, message: "O campo deve ter no mínimo 5 caracteres!" },
+                                    maxLength: { value: 60, message: "O campo deve ter no mínimo 60 caracteres!" }
                                 })}
                                 type="text"
                                 className="form-control input-base"
@@ -75,6 +87,21 @@ const Form = () => {
                                     {errors.name.message}
                                 </div>
                             )}
+                        </div>
+                        <div className="margin-botton-30">
+                            <Controller 
+                                as={Select}
+                                name="categories"
+                                rules={{ required: true }}
+                                control={control}
+                                isLoading={isLoadingCategories}
+                                options={categories} 
+                                getOptionLabel={(option: Category) => option.name}
+                                getOptionValue={(option: Category) => String(option.id)}
+                                classNamePrefix="categories-select"
+                                placeholder="Categoria"
+                                isMulti
+                            />
                         </div>
                         <div className="margin-botton-30">
                             <input
